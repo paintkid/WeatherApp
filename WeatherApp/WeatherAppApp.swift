fd2d589b84f7d92977c211085902163435a86ed5
Joel Kim <joelminchankim@gmail.com>
2025-06-28 08:33:42 -0400

    feat: Initial project setup

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by joel on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
