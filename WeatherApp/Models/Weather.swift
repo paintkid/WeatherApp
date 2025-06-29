dcc5f2fba35e621e2d40682a60ae2b82a597a628
Joel Kim <joelminchankim@gmail.com>
2025-06-28 20:07:13 -0400

    feat: Create Weather model

--- WeatherApp/Models/Weather.swift
+++ WeatherApp/Models/Weather.swift
@@ -1,1 +1,19 @@
+import Foundation
 
+struct WeatherResponse: Codable {
+    let main: MainInfo
+    let weather: [WeatherInfo]
+    let name: String
+}
+
+struct MainInfo: Codable {
+    let temp: Double
+    let temp_min: Double
+    let temp_max: Double
+}
+
+struct WeatherInfo: Codable {
+    let id: Int
+    let main: String
+    let description: String
+}
